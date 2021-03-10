8fdc2d840fe8b0b00811fd0991fbae4d1cd594c8
MominRaza <mominr082@gmail.com>
2021-03-10 19:47:45 +0530

    support for all themes

--- question-content-admin.php
+++ question-content-admin.php
@@ -4,19 +4,51 @@ class question_content_admin
 {
 	public function option_default($option)
 	{
-		if ($option === 'question_content_max_len')
-			return 260;
-	}
+		switch($option) {
 
+			case 'question_content_max_len':
+				return 260;
+
+			case 'question_content_css':
+                return '.question-content-text{
+	font-size: 14px;
+	font-weight: normal;
+	margin-top: 8px;
+	letter-spacing: 0.5px;
+	opacity: 0.8;
+	text-align: justify;
+}
+.question-content-image{
+	width: 100%;
+	aspect-ratio: 16 / 9;
+	object-fit: cover;
+	/* margin: -16px -16px 10px -16px; */
+	margin-bottom: -10px;
+	/* border-radius: 4px 4px 0 0; */
+}';
+			default:
+				return null;				
+		}
+
+	}
 
 	public function admin_form(&$qa_content)
 	{
-		$saved = qa_clicked('question_content_save_button');
+		$ok = null;
 
-		if ($saved) {
+		if (qa_clicked('question_content_save_button')) {
 			qa_opt('question_content_on', (int) qa_post_text('question_content_on_field'));
 			qa_opt('question_content_max_len', (int) qa_post_text('question_content_max_len_field'));
 			qa_opt('question_image_on', (int) qa_post_text('question_image_on_field'));
+			qa_opt('question_content_css', (string) qa_post_text('question_content_css_field'));
+
+			$ok = qa_lang('admin/options_saved');
+		}
+		else if (qa_clicked('question_content_reset_button')) {
+			qa_opt('question_content_max_len', (int) $this->option_default('question_content_max_len'));
+			qa_opt('question_content_css', (string) $this->option_default('question_content_css'));
+
+			$ok = qa_lang('admin/options_reset');
 		}
 
 		qa_set_display_rules($qa_content, array(
@@ -24,7 +56,7 @@ class question_content_admin
 		));
 
 		return array(
-			'ok' => $saved ? 'Question Content settings saved' : null,
+			'ok' => ($ok && !isset($error)) ? $ok : null,
 
 			'fields' => array(
 				array(
@@ -33,7 +65,6 @@ class question_content_admin
 					'value' => qa_opt('question_content_on'),
 					'tags' => 'name="question_content_on_field" id="question_content_on_field"',
 				),
-
 				array(
 					'id' => 'question_content_max_len_display',
 					'label' => 'Maximum length of question content:',
@@ -42,19 +73,29 @@ class question_content_admin
 					'value' => (int) qa_opt('question_content_max_len'),
 					'tags' => 'name="question_content_max_len_field"',
 				),
-
 				array(
 					'label' => 'Shows question\'s first image in question lists',
 					'type' => 'checkbox',
 					'value' => qa_opt('question_image_on'),
 					'tags' => 'name="question_image_on_field" id="question_image_on_field"',
 				),
+				array(
+					'rows' => 8,
+					'label' => 'Question content CSS',
+					'type' => 'textarea',
+					'value' => qa_opt('question_content_css'),
+					'tags' => 'name="question_content_css_field"',
+				),
 			),
 
 			'buttons' => array(
 				array(
-					'label' => 'Save Changes',
+					'label' => qa_lang_html('main/save_button'),
 					'tags' => 'name="question_content_save_button"',
+				),
+				array(
+					'label' => qa_lang_html('admin/reset_options_button'),
+					'tags' => 'name="question_content_reset_button"',
 				),
 			),
 		);
--- question-content-layer.php
+++ question-content-layer.php
@@ -5,23 +5,7 @@ class qa_html_theme_layer extends qa_html_theme_base
 	public function head_custom()
 	{
         qa_html_theme_base::head_custom();
-		$this->output('<style type="text/css">
-		.question-content-text{
-			font-size: 14px;
-			font-weight: normal;
-			margin-top: 8px;
-			letter-spacing: 0.5px;
-			opacity: 0.8;
-			text-align: justify;
-		}
-		.question-content-image{
-			width: calc(100% + 32px);
-			aspect-ratio: 16 / 9;
-			object-fit: cover;
-			margin: -16px -16px 10px -16px;
-			border-radius: 4px 4px 0 0;
-		}
-		</style>');
+		$this->output('<style type="text/css">'.qa_opt('question_content_css').'</style>');
 	}
 
 	public function q_list($q_list)
@@ -65,7 +49,7 @@ class qa_html_theme_layer extends qa_html_theme_base
 							if (!empty($matches[0])) {
 								// assign image to the variable
 								$image = '<img src="' . $matches[1][0] . '" alt="'.$title.'" class="question-content-image" />'; // change the size using attr or css
-								$q_list['qs'][$index]['content'] = '<a href="'.qa_path_html($question['raw']['postid']).'" >'.$image.'</a>';
+								$q_list['qs'][$index]['imagecontent'] = '<a href="'.qa_path_html($question['raw']['postid']).'" >'.$image.'</a>';
 							}
 						}
 					}
@@ -75,25 +59,15 @@ class qa_html_theme_layer extends qa_html_theme_base
 
 		parent::q_list($q_list); // call back through to the default function
 	}
-
-	public function q_item_main($q_item)
+	public function q_list_item($q_item)
 	{
-		if ($this->template != 'admin' && qa_opt('question_image_on')) {
-			$this->output('<div class="qa-q-item-main">');
-				
-			$this->q_item_content($q_item);
-			$this->post_avatar_meta($q_item, 'qa-q-item');
-			$this->q_item_title($q_item);
-			$this->post_tags($q_item, 'qa-q-item');
-			$this->q_item_buttons($q_item);
-
-			$this->output('</div>');
-		} else {
-			parent::q_item_main($q_item);
-		}	
+		if (!empty($q_item['imagecontent']))
+			$this->output_raw($q_item['imagecontent']);
+		
+		parent::q_list_item($q_item);
 	}
 	
-	private function qam_shorten_string_line($string, $length, $ellipsis = '...')
+	private function qam_shorten_string_line($string, $length, $ellipsis = ' ...')
 	{
 		if (qa_strlen($string) > $length) {
 			$remaining = $length - qa_strlen($ellipsis);
